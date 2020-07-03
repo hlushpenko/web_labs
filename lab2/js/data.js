eb463f7f9450b6d8cedb3803de2ac8e1511eddaf
Nick Hlushpenko <nick.hlushpenko@gmail.com>
2020-07-03 11:34:05 +0300

    add lab 2, bulma

---
+++ lab2/js/data.js
@@ -0,0 +1,14 @@
+function saveGroups(groups) {
+    localStorage.setItem("groups_data", JSON.stringify(groups));
+}
+
+var groups = localStorage.getItem('groups_data');
+if (localStorage.getItem("groups_data") === null) {
+    groups = [
+        {number: 301, faculty: 'Computer science'},
+        {number: 302, faculty: 'Computer network'}
+    ];
+    localStorage.setItem("groups_data", JSON.stringify(groups));
+} else {
+    groups = JSON.parse(groups);
+}
---
+++ lab2/js/my.js
@@ -0,0 +1,42 @@
+$("document").ready(function () {
+    var rowText;
+    var content = $('#groups');
+    for (var row of groups) {
+        rowText =
+            `<tr>
+                <td>${row.number}</td>
+                <td>  ${row.faculty}</td>
+                <td class="has-text-right">
+                    <a class="button is-link" href="group.html?number=${row.number}">
+                        <i class="fa fa-fw fa-edit"></i>
+                    </a>
+                    <button type = "button" class="button is-danger rem-row" rowid="${row.number}">
+                        <i class="fa fa-fw fa-trash"></i>
+                    </button>
+                </td>  
+            </tr>`;
+        content.append(rowText);
+    }
+    $('.rem-row').click(function () {
+        let number = $(this).attr('rowid');
+        saveGroups(groups.filter((g)=>g.number !== number));
+        location.reload();
+    });
+
+
+
+
+    $('#click-me').click(function () {
+        var html = `<div class="text-left alert alert-success" role="alert">
+          <h4 class="alert-heading"> Привіт!
+      <button type="button" class="close" data-dismiss="alert" aria-label="close">
+          <span aria-hidden="true"> &times; </span>
+      </button>
+      </h4>
+      <p>Вітаємо з підключенням Twitter BootStrap</p>
+      <hr>
+      <p>Можна приступати до роботи</p>
+      </div>`;
+        $('#alert').html(html);
+    });
+});
